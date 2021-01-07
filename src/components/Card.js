49b49ab87c1ebdaeedabb9539398341aa6efac16
FernanEd <fernandoed.bm@gmail.com>
2021-01-07 01:49:52 -0600

    I tried to make a loading but it doesnt work as I expected, I need to practice async code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,9 @@
 import { useState, useEffect } from 'react';
 
-export default function Card({ pokemonIndex, evaluateCardIndex }) {
-  const [imgSrc, setImgSrc] = useState('');
-
-  useEffect(async () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`)
-      .then((data) => data.json())
-      .then((pokemon) => pokemon.sprites)
-      .then((sprite) => sprite.front_default)
-      .then((src) => setImgSrc(src))
-      .catch((e) => {});
-  }, [pokemonIndex]);
+export default function Card({ pokemonIndex, evaluateCardIndex, imgSrc }) {
+  useEffect(() => {
+    console.log(imgSrc);
+  }, []);
 
   return (
     <div className="card" onClick={() => evaluateCardIndex(pokemonIndex)}>
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,7 +4,11 @@ import Card from './Card';
 export default function Deck({ addScore, clearScore, pokedexLength }) {
   const [seenPokemons, setSeenPokemons] = useState([]);
   const [generatedPokemons, setGeneratedPokemons] = useState([]);
+  const [imagesUrl, setImagesUrl] = useState([]);
   const [numberOfCards, setNumberOfCards] = useState(3);
+  const [isLoading, setIsLoading] = useState(true);
+
+  //Every time a new batch is generated
 
   const generateNewBatch = () => {
     let newBatch = [];
@@ -21,6 +25,9 @@ export default function Deck({ addScore, clearScore, pokedexLength }) {
       }
       newBatch.push(randomIndex);
     }
+
+    //Shuffle newBatch array randomly
+    newBatch.sort((a, b) => 0.5 - Math.random());
 
     setGeneratedPokemons(newBatch);
   };
@@ -47,16 +54,37 @@ export default function Deck({ addScore, clearScore, pokedexLength }) {
     generateNewBatch();
   }, []);
 
+  useEffect(async () => {
+    setIsLoading(true);
+    let images = [];
+
+    for (let pokemonIndex of generatedPokemons) {
+      let response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`
+      );
+      let data = await response.json();
+      let sprite = data.sprites;
+      let imgSrc = sprite.front_default;
+      images.push(imgSrc);
+    }
+
+    setImagesUrl(images);
+    setIsLoading(false);
+  }, [generatedPokemons]);
+
   return (
     <div>
       <div className="card-wrapper">
-        {[...new Array(numberOfCards)].map((e, cardIndex) => (
-          <Card
-            key={cardIndex}
-            pokemonIndex={generatedPokemons[cardIndex]}
-            evaluateCardIndex={evaluateCardIndex}
-          />
-        ))}
+        {isLoading
+          ? 'Loading...'
+          : [...new Array(numberOfCards)].map((e, cardIndex) => (
+              <Card
+                key={cardIndex}
+                pokemonIndex={generatedPokemons[cardIndex]}
+                evaluateCardIndex={evaluateCardIndex}
+                imgSrc={imagesUrl[cardIndex]}
+              />
+            ))}
       </div>
     </div>
   );
